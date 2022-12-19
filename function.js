e768b3b5ef979b038af0e9dc0933259e535de783
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-19 23:38:23 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -2,37 +2,24 @@ Office.initialize = function () {
     // Fonction exécutée lorsque l'add-in est chargé
 };
 
-function SendToSpecificEmail() {
-    // Make sure the script is running in Outlook on the web
-    if (Office.context.mailbox.diagnostics.hostName !== 'OutlookIOS' && Office.context.mailbox.diagnostics.platform !== 'OutlookWebApp') {
-        UI.notify('This script can only be run in Outlook on the web.', 'error');
-        return;
-    }
+function sendAndDeleteMessage() {
+    // Récupère le message sélectionné
+    var item = Office.context.mailbox.item;
 
-    // Get the current mailbox and selected message
-    var mailbox = Office.context.mailbox;
-    var item = mailbox.item;
+    // Envoie le message à l'adresse e-mail spécifiée
+    item.forwardAsync("mathis.merme@gmail.com", {
+        asyncContext: { message: item }
+    }, function (asyncResult) {
+        if (asyncResult.status == "failed") {
+            console.error("Error sending message: " + asyncResult.error.message);
+            return;
+        }
 
-    // Get the value of the saveToSentItems option
-    var saveToSentItems = document.getElementById('saveToSentItems').value;
-
-    // Forward the message to the specified email address and delete it
-    try {
-        item.forwardAsync("mathis.merme@gmail.com", { saveToSentItems: saveToSentItems },
-            function (result) {
-                if (result.status === 'failed') {
-                    UI.notify(`An error occurred while forwarding the message: ${result.error.message}`, 'error');
-                } else {
-                    item.deleteAsync(function (deleteResult) {
-                        if (deleteResult.status === 'failed') {
-                            UI.notify(`An error occurred while deleting the message: ${deleteResult.error.message}`, 'error');
-                        } else {
-                            UI.notify('The message was forwarded and deleted successfully.', 'success');
-                        }
-                    });
-                }
-            });
-    } catch (error) {
-        UI.notify(`An unexpected error occurred: ${error.message}`, 'error');
-    }
+        // Supprime le message une fois l'envoi terminé
+        asyncResult.value.deleteAsync(function (asyncResult) {
+            if (asyncResult.status == "failed") {
+                console.error("Error deleting message: " + asyncResult.error.message);
+            }
+        });
+    });
 }
