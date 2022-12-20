d03aeeaf9b3d42854459355cf2a3d3e40e820887
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 14:00:24 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -16,40 +16,6 @@
     // defining global variables to pass them to the composeMail function
     var phishItemId;
     var phishSubject;
-    var receipentMailAddress;
-
-
-
-    
-    // get the reciepent or ask to enter value
-    // check if there is an email address set to send the mail to
-    function securityTeamMailAddress() {
-        //check if email is already set
-        if (Office.context.roamingSettings.get("email")) {
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-        }
-        // show popup to enter the email address to report phishing to.
-        else {
-            // TODO: Create popup to enter email at first run
-
-           // Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-            saveRoamingSettings()
-        }
-    }
-
-    // save value's to roaming settings so it can be accessed later
-    //function saveRoamingSettings() {
-        // Save settings in the mailbox to make it available in future sessions.
-        //Office.context.roamingSettings.saveAsync(function (result) {
-            //if (result.status !== Office.AsyncResultStatus.Succeeded) {
-                //console.error(`Action failed with message ${result.error.message}`);
-            //} else {
-                //console.log(`Settings saved with status: ${result.status}`);
-            //}
-        //});
-    //}
-
 
     // this function has to run before composing a new mail to retrieve the details of the current selected email. 
     function getPhishingItem(item) {
@@ -80,26 +46,3 @@ function hideShowSettings() {
         document.getElementById("settings").style.display = "none";
     };
 };
-
-function loadCurrentMailAddress() {
-    // Write message property values to the task pane
-    document.getElementById("currentMailAddress").innerHTML = Office.context.roamingSettings.get("email");
-}
-
-function changeMailAddress() {
-    var newMailAddress = document.getElementById("newMailAddress").value;
-    Office.context.roamingSettings.set("email", newMailAddress);
-    saveRoamingSettings();
-}
-
-function saveRoamingSettings() {
-    // Save settings in the mailbox to make it available in future sessions.
-    Office.context.roamingSettings.saveAsync(function (result) {
-        if (result.status !== Office.AsyncResultStatus.Succeeded) {
-            console.error(`Action failed with message ${result.error.message}`);
-        } else {
-            console.log(`Settings saved with status: ${result.status}`);
-            loadCurrentMailAddress()
-        }
-    });
-}
