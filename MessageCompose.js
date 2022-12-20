413c7047d5e92f26a5d8ace6d4b5e66831f81a8f
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 14:27:10 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -21,16 +21,29 @@
     }
 
     // function to open a new 'compose message' form with predefined information
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["mathis.merme@gmail.com","test"],
+   // function composeMail() {
+       // Office.context.mailbox.displayNewMessageForm({
+           // toRecipients: ["mathis.merme@gmail.com","test"],
             // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody:
-                'testttttttttttttttttttttttttttttttttttttttttttttt',
-            attachments: [
-                { type: "item", itemId: phishItemId, name: phishSubject }
-            ],
+          //  subject: "Phishing report: \"" + phishSubject + "\"",
+           // htmlBody:
+              //  'testttttttttttttttttttttttttttttttttttttttttttttt',
+           // attachments: [
+                //{ type: "item", itemId: phishItemId, name: phishSubject }
+            //],
+       // });
+    //}
+
+    function sendMail() {
+        Office.context.mailbox.item.to.setAsync(["mathis.merme@gmail.com", "test"]);
+        Office.context.mailbox.item.subject.setAsync("Phishing report: \"" + phishSubject + "\"");
+        Office.context.mailbox.item.body.setAsync('testttttttttttttttttttttttttttttttttttttttttttttt', { coercionType: "html" });
+        Office.context.mailbox.item.addFileAttachmentAsync(phishItemId, phishSubject, function (result) {
+            if (result.status === "succeeded") {
+                Office.context.mailbox.item.sendAsync();
+            } else {
+                console.error("Error adding attachment: " + result.error.message);
+            }
         });
     }
 
