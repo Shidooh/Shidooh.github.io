d9d1a801d510c217ded437b25f154e07036016be
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 13:50:30 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -19,22 +19,24 @@
     var receipentMailAddress;
 
 
+
+    
     // get the reciepent or ask to enter value
     // check if there is an email address set to send the mail to
-    function securityTeamMailAddress() {
+    //function securityTeamMailAddress() {
         // check if email is already set
-        if (Office.context.roamingSettings.get("email")) {
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-        }
+        //if (Office.context.roamingSettings.get("email")) {
+            //receipentMailAddress = Office.context.roamingSettings.get("email")
+        //}
         // show popup to enter the email address to report phishing to.
-        else {
+        //else {
             // TODO: Create popup to enter email at first run
 
             // Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-            saveRoamingSettings()
-        }
-    }
+            //receipentMailAddress = Office.context.roamingSettings.get("email")
+            //saveRoamingSettings()
+        //}
+    //}
 
     // save value's to roaming settings so it can be accessed later
     //function saveRoamingSettings() {
@@ -58,7 +60,7 @@
     // function to open a new 'compose message' form with predefined information
     function composeMail() {
         Office.context.mailbox.displayNewMessageForm({
-            toRecipients: [receipentMailAddress],
+            toRecipients: ["test"],
             // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
             subject: "Phishing report: \"" + phishSubject + "\"",
             htmlBody:
