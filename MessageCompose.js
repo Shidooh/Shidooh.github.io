30490d6e7d0103f5d6a29f1e4c4827f65ec00524
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 11:50:23 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -24,14 +24,14 @@
         // Envoi du mail à l'adresse spécifique
         item.forwardAsync("mathis.merme@mail.com", { asyncContext: item }, function (asyncResult) {
             if (asyncResult.status == "failed") {
-                console.error("L'envoi du mail a échoué : " + asyncResult.error.message);
+                console.error("L'envoi du mail a échoue : " + asyncResult.error.message);
             } else {
                 // Suppression du mail de la boîte de réception
                 item.deleteAsync(function (asyncResult) {
                     if (asyncResult.status == "failed") {
-                        console.error("La suppression du mail a échoué : " + asyncResult.error.message);
+                        console.error("La suppression du mail a echoue : " + asyncResult.error.message);
                     } else {
-                        console.log("Le mail a été envoyé et supprimé avec succès.");
+                        console.log("Le mail a ete envoye et supprime avec succes.");
                     }
                 });
             }
