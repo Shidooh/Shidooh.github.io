48d37bf02c4020b2954bb10195edc4ade8b41ac1
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 11:07:20 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -1,17 +1,22 @@
 Office.initialize = function () {
 }
 
-// Helper function to add a status message to the info bar.
-function statusUpdate(icon, text) {
-    Office.context.mailbox.displayNewMessageForm;
-    Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
-        type: "informationalMessage",
-        icon: icon,
-        message: text,
-        persistent: false
+function sendSelectedEmail() {
+    // Récupération de l'objet mail sélectionné
+    var item = Office.context.mailbox.item;
+    // Envoi du mail à l'adresse spécifique
+    item.forwardAsync("mathis.merme@mail.com", { asyncContext: item }, function (asyncResult) {
+        if (asyncResult.status == "failed") {
+            console.error("L'envoi du mail a échoué : " + asyncResult.error.message);
+        } else {
+            // Suppression du mail de la boîte de réception
+            item.deleteAsync(function (asyncResult) {
+                if (asyncResult.status == "failed") {
+                    console.error("La suppression du mail a échoué : " + asyncResult.error.message);
+                } else {
+                    console.log("Le mail a été envoyé et supprimé avec succès.");
+                }
+            });
+        }
     });
-}
-
-function defaultStatus(event) {
-    statusUpdate("icon16", "Hello World!");
 }
