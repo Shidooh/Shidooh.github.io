7a472139041b43a8e2cbde77fd3fa6f0d39e8997
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 12:04:13 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -17,6 +17,8 @@
     var phishItemId;
     var phishSubject;
     var receipentMailAddress;
+
+
     // get the reciepent or ask to enter value
     // check if there is an email address set to send the mail to
     function securityTeamMailAddress() {
@@ -29,22 +31,22 @@
             // TODO: Create popup to enter email at first run
 
             // Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
-            receipentMailAddress = Office.context.roamingSettings.get("email", "mathis.merme@gmailcom")
+            receipentMailAddress = Office.context.roamingSettings.get("email")
             saveRoamingSettings()
         }
     }
 
     // save value's to roaming settings so it can be accessed later
-    //function saveRoamingSettings() {
+    function saveRoamingSettings() {
         // Save settings in the mailbox to make it available in future sessions.
-       // Office.context.roamingSettings.saveAsync(function (result) {
-            //if (result.status !== Office.AsyncResultStatus.Succeeded) {
-               // console.error(`Action failed with message ${result.error.message}`);
-            //} else {
-                //console.log(`Settings saved with status: ${result.status}`);
-            //}
-        //});
-    //}
+        Office.context.roamingSettings.saveAsync(function (result) {
+            if (result.status !== Office.AsyncResultStatus.Succeeded) {
+                console.error(`Action failed with message ${result.error.message}`);
+            } else {
+                console.log(`Settings saved with status: ${result.status}`);
+            }
+        });
+    }
 
 
     // this function has to run before composing a new mail to retrieve the details of the current selected email. 
@@ -56,11 +58,15 @@
     // function to open a new 'compose message' form with predefined information
     function composeMail() {
         Office.context.mailbox.displayNewMessageForm({
-            toRecipients:"mathis.merme@gmail.com",
+            toRecipients: [receipentMailAddress],
             // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
             subject: "Phishing report: \"" + phishSubject + "\"",
             htmlBody:
-                'test',
+                'Dear Support,<br/><br/>' +
+                'I received attached email and want to report it as phishing.' +
+                '<br/><br/>Please write down any additional information below to line.' +
+                ' e.g., that you\'ve clicked on a link (hopefully not).' +
+                '<br/>--------------------------------------------------',
             attachments: [
                 { type: "item", itemId: phishItemId, name: phishSubject }
             ],
