b8fc06e9a69153de21e0a38e40427403ba129d08
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 15:54:17 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -19,36 +19,6 @@
     var receipentMailAddress;
 
 
-    // get the reciepent or ask to enter value
-    // check if there is an email address set to send the mail to
-    function securityTeamMailAddress() {
-        // check if email is already set
-        if (Office.context.roamingSettings.get("email")) {
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-        }
-        // show popup to enter the email address to report phishing to.
-        else {
-            // TODO: Create popup to enter email at first run
-
-            // Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
-            receipentMailAddress = Office.context.roamingSettings.get("email")
-            saveRoamingSettings()
-        }
-    }
-
-    // save value's to roaming settings so it can be accessed later
-    function saveRoamingSettings() {
-        // Save settings in the mailbox to make it available in future sessions.
-        Office.context.roamingSettings.saveAsync(function (result) {
-            if (result.status !== Office.AsyncResultStatus.Succeeded) {
-                console.error(`Action failed with message ${result.error.message}`);
-            } else {
-                console.log(`Settings saved with status: ${result.status}`);
-            }
-        });
-    }
-
-
     // this function has to run before composing a new mail to retrieve the details of the current selected email. 
     function getPhishingItem(item) {
         phishItemId = item.itemId
