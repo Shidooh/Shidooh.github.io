a387639659825901c7698354ca9946364b7bff63
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-20 16:45:31 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -51,7 +51,7 @@
             // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
             subject: "Phishing report: \"" + phishSubject + "\"",
             htmlBody:
-                'nope',
+                'nopttttttttttttte',
             attachments: [
                 { type: "item", itemId: phishItemId, name: phishSubject }
             ],
