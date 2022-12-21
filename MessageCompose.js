97228e1359ec4510e989a959febece34cfc3bbf5
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 12:19:24 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -6,17 +6,15 @@
         // Office is ready
         $(document).ready(function () {
             getPhishingItem(Office.context.mailbox.item);
-            composeMail();
-            //run();
-            //runAsync();
+            //composeMail();
+            run();
+            runAsync();
         });
     });
 
     // defining global variables to pass them to the composeMail function
     var phishItemId;
     var phishSubject;
-    var email = 'mathis.merme@mail.com';
-
 
 
 function getPhishingItem(item) {
@@ -24,37 +22,37 @@ function getPhishingItem(item) {
         phishSubject = item.subject
     }
 
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm ({
-            toRecipients: ["mathis.merme@gmail.com"],
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody:'testttttttttttt',
-            attachments: [ { type: "item", itemId: phishItemId, name: phishSubject } ],
-        });
+//    function composeMail() {
+//        Office.context.mailbox.displayNewMessageForm ({
+//            toRecipients: ["mathis.merme@gmail.com"],
+//            subject: "Phishing report: \"" + phishSubject + "\"",
+//            htmlBody:'testttttttttttt',
+//            attachments: [ { type: "item", itemId: phishItemId, name: phishSubject } ],
+//        });
+//    }
+
+//})();
+
+function run() {
+    Office.context.mailbox.displayNewMessageForm({
+        toRecipients: ["mathis.merme@gmail.com"],
+        subject: "Outlook add-ins are cool!",
+        htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
+        attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+    });
+}
+
+    function runAsync() {
+        Office.context.mailbox.displayNewMessageFormAsync(
+            {
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Outlook add-ins are cool!",
+                htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+            },
+            function (asyncResult) {
+                console.log(JSON.stringify(asyncResult));
+            }
+        );
     }
-
-})();
-
-//function run() {
-//    Office.context.mailbox.displayNewMessageForm({
-//        toRecipients: ["mathis.merme@gmail.com"],
-//        subject: "Outlook add-ins are cool!",
-//        htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
-//        attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-//    });
-//}
-
-    //function runAsync() {
-    //    Office.context.mailbox.displayNewMessageFormAsync(
-    //        {
-    //            toRecipients: ["mathis.merme@gmail.com"],
-    //            subject: "Outlook add-ins are cool!",
-    //            htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
-    //            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-    //        },
-    //        function (asyncResult) {
-    //            console.log(JSON.stringify(asyncResult));
-    //        }
-    //    );
-    //}
-    //})();
+    })();
