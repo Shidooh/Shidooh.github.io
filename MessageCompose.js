5ea6ad3d57fb760ae74d12b4eaf15d62d43d2e74
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 10:36:56 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -6,7 +6,9 @@
         // Office is ready
         $(document).ready(function () {
             getPhishingItem(Office.context.mailbox.item);
-            composeMail();
+            //composeMail();
+            run();
+            runAsync();
         });
     });
 
@@ -17,18 +19,42 @@
 
 
 
-function getPhishingItem(item) {
-        phishItemId = item.itemId
-        phishSubject = item.subject
-    }
+//function getPhishingItem(item) {
+//        phishItemId = item.itemId
+//        phishSubject = item.subject
+//    }
 
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm.send({
-            toRecipients: ["mathis.merme@gmail.com"],
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody:'testttttttttttt',
-            attachments: [ { type: "item", itemId: phishItemId, name: phishSubject } ],
-        });
-    }
+//    function composeMail() {
+//        Office.context.mailbox.displayNewMessageForm ({
+//            toRecipients: ["mathis.merme@gmail.com"],
+//            subject: "Phishing report: \"" + phishSubject + "\"",
+//            htmlBody:'testttttttttttt',
+//            attachments: [ { type: "item", itemId: phishItemId, name: phishSubject } ],
+//        });
+//    }
 
-})();
+//})();
+
+function run() {
+    Office.context.mailbox.displayNewMessageForm({
+        toRecipients: ["mathis.merme@gmail.com"],
+        subject: "Outlook add-ins are cool!",
+        htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
+        attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+    });
+}
+
+    function runAsync() {
+        Office.context.mailbox.displayNewMessageFormAsync(
+            {
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Outlook add-ins are cool!",
+                htmlBody: 'Hello <b>World</b>!<br/><img src="cid:image.png"></i>',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+            },
+            function (asyncResult) {
+                console.log(JSON.stringify(asyncResult));
+            }
+        );
+    }
+    })();
