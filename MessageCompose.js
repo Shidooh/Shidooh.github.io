3d5d1601fcc762b1191d83961dff080702e78006
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 10:21:34 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -5,7 +5,6 @@
     Office.onReady(function () {
         // Office is ready
         $(document).ready(function () {
-            //test();
             getPhishingItem(Office.context.mailbox.item);
             composeMail();
         });
@@ -14,46 +13,8 @@
     // defining global variables to pass them to the composeMail function
     var phishItemId;
     var phishSubject;
-    var receipentMailAddress;
     var email = 'mathis.merme@mail.com';
 
-    //function test() {
-    //    Office.context.mailbox.item.create(
-    //    "message",
-    //    {
-    //        toRecipients: ["mathis.merme@grmail.com"],
-    //        subject: "Outlook add-ins are cool!",
-    //        body: {
-    //            contentType: "html",
-    //            content: 'Hello <b>World</b>!'
-    //        }
-    //    },
-    //    function (result) {
-    //        // The create callback function
-    //        if (result.status === "created") {
-    //            var item = result.value;
-    //            item.send();
-    //        }
-    //    }
-    //);
-    //}
-
-    // get the reciepent or ask to enter value
-    // check if there is an email address set to send the mail to
-    //function securityTeamMailAddress() {
-    //     check if email is already set
-    //    if (Office.context.roamingSettings.get("email")) {
-    //        receipentMailAddress = Office.context.roamingSettings.get("email")
-    //    }
-    //     show popup to enter the email address to report phishing to.
-    //    else {
-    //         TODO: Create popup to enter email at first run
-
-    //         Office.context.roamingSettings.set("email", "j.vdvelden99@gmail.com")
-    //        receipentMailAddress = Office.context.roamingSettings.get("email")
-    //        saveRoamingSettings()
-    //    }
-    //}
 
 
 function getPhishingItem(item) {
@@ -61,20 +22,13 @@ function getPhishingItem(item) {
         phishSubject = item.subject
     }
 
-    
-    // this function has to run before composing a new mail to retrieve the details of the current selected email. 
-    
-
-
-    // function to open a new 'compose message' form with predefined information
     function composeMail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["mathis.merme@gmail.com", "test"],
+        Office.context.mailbox.displayNewMessageForm.send({
+            toRecipients: ["mathis.merme@gmail.com"],
             subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody:'test',
+            htmlBody:'testttttttttttt',
             attachments: [ { type: "item", itemId: phishItemId, name: phishSubject } ],
         });
-        displayNewMessageForm.send();
     }
 
 })();
