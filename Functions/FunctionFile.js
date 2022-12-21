271aa27d8b28b26918b7e169b641f7ea60c07c75
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 16:17:59 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -26,7 +26,7 @@
             Office.context.mailbox.displayNewMessageForm({
                 toRecipients: ["mathis.merme@gmail.com"],
                 subject: "Phishing report: \"" + phishSubject + "\"",
-                htmlBody: 'test',
+                htmlBody: 'testttttttttttttttttttt',
                 attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
             });
             phishItemId.deleteAsync()
