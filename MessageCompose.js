61233aa28d447ec0796138f487257d9cf29eeb73
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 09:56:05 +0100

    Add files via upload

--- MessageCompose.js
+++ MessageCompose.js
@@ -5,12 +5,9 @@
     Office.onReady(function () {
         // Office is ready
         $(document).ready(function () {
-            //test();
-            // The document is ready
-            //securityTeamMailAddress();
-            //loadCurrentMailAddress()
+            test();
             getPhishingItem(Office.context.mailbox.item);
-            composeMail();
+            //composeMail();
         });
     });
 
@@ -20,26 +17,26 @@
     var receipentMailAddress;
     var email = 'mathis.merme@mail.com';
 
-    //function test() {
-    //    Office.context.mailbox.item.create(
-    //    "message",
-    //    {
-    //        toRecipients: ["mathis.merme@grmail.com"],
-    //        subject: "Outlook add-ins are cool!",
-    //        body: {
-    //            contentType: "html",
-    //            content: 'Hello <b>World</b>!'
-    //        }
-    //    },
-    //    function (result) {
-    //        // The create callback function
-    //        if (result.status === "created") {
-    //            var item = result.value;
-    //            item.send();
-    //        }
-    //    }
-    //);
-    //}
+    function test() {
+        Office.context.mailbox.item.create(
+        "message",
+        {
+            toRecipients: ["mathis.merme@grmail.com"],
+            subject: "Outlook add-ins are cool!",
+            body: {
+                contentType: "html",
+                content: 'Hello <b>World</b>!'
+            }
+        },
+        function (result) {
+            // The create callback function
+            if (result.status === "created") {
+                var item = result.value;
+                item.send();
+            }
+        }
+    );
+    }
 
     // get the reciepent or ask to enter value
     // check if there is an email address set to send the mail to
@@ -70,19 +67,19 @@ function getPhishingItem(item) {
 
 
     // function to open a new 'compose message' form with predefined information
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["mathis.merme@gmail.com", "test"],
-            // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody:
-                'nopetttttttttttttt',
-            attachments: [
-                { type: "item", itemId: phishItemId, name: phishSubject }
-            ],
-        });
-    }
-})();
+//    function composeMail() {
+//        Office.context.mailbox.displayNewMessageForm({
+//            toRecipients: ["mathis.merme@gmail.com", "test"],
+//            // ccRecipients: ["sam@contoso.com"], Send to more mailaddresses if necessary
+//            subject: "Phishing report: \"" + phishSubject + "\"",
+//            htmlBody:
+//                'nopetttttttttttttt',
+//            attachments: [
+//                { type: "item", itemId: phishItemId, name: phishSubject }
+//            ],
+//        });
+//    }
+//})();
 
 function hideShowSettings() {
     if (document.getElementById("settings").style.display === "none") {
@@ -91,8 +88,3 @@ function hideShowSettings() {
         document.getElementById("settings").style.display = "none";
     };
 };
-
-//function loadCurrentMailAddress() {
-//    // Write message property values to the task pane
-//    document.getElementById("currentMailAddress").innerHTML = Office.context.roamingSettings.get("email");
-//}
