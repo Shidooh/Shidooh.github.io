041079fb29da66eac5e9cffef49215cfa5c091d9
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 15:41:20 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -2,56 +2,60 @@
 
 (function () {
 
-    Office.onReady(function () {
-        // Office is ready
-        $(document).ready(function () {
-            getPhishingItem(Office.context.mailbox.item);
-            composeMail();
+        Office.onReady(function () {
+            // Office is ready
+            $(document).ready(function () {
+                getPhishingItem(Office.context.mailbox.item);
+                composeMail();
+            });
         });
-    });
 
-    // defining global variables to pass them to the composeMail function
-    var phishItemId;
-    var phishSubject;
-
-
-    function getPhishingItem(item) {
-        phishItemId = item.itemId
-        phishSubject = item.subject
-    }
-
-    function composeMail() {
-        Office.context.mailbox.item({
-            toRecipients: ["mathis.merme@gmail.com"],
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody: 'TEST',
-            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-        });
-    }
-
-})();
+        // defining global variables to pass them to the composeMail function
+        var phishItemId;
+        var phishSubject;
+        var item = office.context.mailbox.item;
 
 
+        function getPhishingItem(item) {
+            phishItemId = item.itemId
+            phishSubject = item.subject
+        }
 
-//Office.initialize = function () {
-//}
+        function composeMail() {
+            Office.context.mailbox.displayNewMessageForm({
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Phishing report: \"" + phishSubject + "\"",
+                htmlBody: 'TEST',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+            });
+        }
 
-//function sendSelectedEmail() {
-//    // Récupération de l'objet mail sélectionné
-//    var item = Office.context.mailbox.item;
-//    // Envoi du mail à l'adresse spécifique
-//    item.forwardAsync("mathis.merme@mail.com", { asyncContext: item }, function (asyncResult) {
-//        if (asyncResult.status == "failed") {
-//            console.error("L'envoi du mail a échoué : " + asyncResult.error.message);
-//        } else {
-//            // Suppression du mail de la boîte de réception
-//            item.deleteAsync(function (asyncResult) {
-//                if (asyncResult.status == "failed") {
-//                    console.error("La suppression du mail a échoué : " + asyncResult.error.message);
-//                } else {
-//                    console.log("Le mail a été envoyé et supprimé avec succès.");
-//                }
-//            });
-//        }
-//    });
-//}
+    })();
+
+
+
+
+//    Office.initialize = function () {
+//        sendSelectedEmail();
+//    }
+
+//    function sendSelectedEmail() {
+//        // Récupération de l'objet mail sélectionné
+//        var item = Office.context.mailbox.item;
+//        // Envoi du mail à l'adresse spécifique
+//        item.forwardAsync("mathis.merme@mail.com", { asyncContext: item }, function (asyncResult) {
+//            if (asyncResult.status == "failed") {
+//                console.error("L'envoi du mail a échoué : " + asyncResult.error.message);
+//            } else {
+//                // Suppression du mail de la boîte de réception
+//                item.deleteAsync(function (asyncResult) {
+//                    if (asyncResult.status == "failed") {
+//                        console.error("La suppression du mail a échoué : " + asyncResult.error.message);
+//                    } else {
+//                        console.log("Le mail a été envoyé et supprimé avec succès.");
+//                    }
+//                });
+//            }
+//        });
+//    }
+//})();
