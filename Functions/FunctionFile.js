f664cd17f36c4522f377b7d377b7a0e8cc24c779
Shidooh <100126517+Shidooh@users.noreply.github.com>
2022-12-21 15:10:51 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -1,58 +1,17 @@
-'use strict';
+ï»¿Office.initialize = function () {
+}
 
-(function () {
+// Helper function to add a status message to the info bar.
+function statusUpdate(icon, text) {
+    Office.context.mailbox.displayNewMessageForm;
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
 
-    Office.onReady(function () {
-        // Office is ready
-        $(document).ready(function () {
-            getPhishingItem(Office.context.mailbox.item);
-            composeMail();
-            //run();
-            //runAsync();
-        });
-    });
-
-    // defining global variables to pass them to the composeMail function
-    var phishItemId;
-    var phishSubject;
-
-
-    function getPhishingItem(item) {
-        phishItemId = item.itemId
-        phishSubject = item.subject
-    }
-
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["mathis.merme@gmail.com"],
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody: 'TEST',
-            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-        });
-    }
-
-})();
-
-
-//Office.initialize = function () {
-//}
-
-//function sendSelectedEmail() {
-//    // Récupération de l'objet mail sélectionné
-//    var item = Office.context.mailbox.item;
-//    // Envoi du mail à l'adresse spécifique
-//    item.forwardAsync("mathis.merme@mail.com", { asyncContext: item }, function (asyncResult) {
-//        if (asyncResult.status == "failed") {
-//            console.error("L'envoi du mail a échoué : " + asyncResult.error.message);
-//        } else {
-//            // Suppression du mail de la boîte de réception
-//            item.deleteAsync(function (asyncResult) {
-//                if (asyncResult.status == "failed") {
-//                    console.error("La suppression du mail a échoué : " + asyncResult.error.message);
-//                } else {
-//                    console.log("Le mail a été envoyé et supprimé avec succès.");
-//                }
-//            });
-//        }
-//    });
-//}
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
