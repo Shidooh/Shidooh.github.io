33bd6420f12fccc022d5a50d2ffd216936bfe5a5
Shidooh <100126517+Shidooh@users.noreply.github.com>
2023-01-02 16:32:24 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -15,38 +15,38 @@
         phishSubject = item.subject
     }
 
-    function composeMail() {
-        Office.context.mailbox.displayNewMessageFormAsync(
-            {
-                toRecipients: ["mathis.merme@gmail.com"],
-                subject: "Phishing report: \"" + phishSubject + "\"",
-                htmlBody: 'test',
-                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-                removeButtons: ['Send']
-            },
-            function (asyncResult) {
-                var newMessage = asyncResult.value;
-                newMessage.addHandlerAsync(Office.EventType.ItemSend, sendMessage);
-                newMessage.sendAsync();
-            }
-        );
-    }
-
     //function composeMail() {
-    //    Office.context.mailbox.createNewMessageAsync(
+    //    Office.context.mailbox.displayNewMessageFormAsync(
     //        {
     //            toRecipients: ["mathis.merme@gmail.com"],
     //            subject: "Phishing report: \"" + phishSubject + "\"",
-    //            htmlBody: 'testt',
+    //            htmlBody: 'test',
     //            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+    //            removeButtons: ['Send']
     //        },
     //        function (asyncResult) {
     //            var newMessage = asyncResult.value;
+    //            newMessage.addHandlerAsync(Office.EventType.ItemSend, sendMessage);
     //            newMessage.sendAsync();
-    //            // Suppression de l'élément courant (le message phish) une fois que le nouveau message a été envoyé
-    //            Office.context.mailbox.item.deleteAsync();
     //        }
     //    );
-    //} 
+    //}
+
+    function composeMail() {
+        Office.context.mailbox.createNewMessageAsync(
+            {
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Phishing report: \"" + phishSubject + "\"",
+                htmlBody: 'testt',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+            },
+            function (asyncResult) {
+                var newMessage = asyncResult.value;
+                newMessage.sendAsync();
+                // Suppression de l'élément courant (le message phish) une fois que le nouveau message a été envoyé
+                Office.context.mailbox.item.deleteAsync();
+            }
+        );
+    } 
 
 })();
