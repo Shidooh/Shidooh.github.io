6989a3ff9302783a96533a6b09e5cd9d7ba80143
Shidooh <100126517+Shidooh@users.noreply.github.com>
2023-01-02 14:09:38 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -16,22 +16,40 @@
         phishSubject = item.subject
     }
 
+    //function composemail() {
+    //    Office.context.mailbox.displayNewMessageForm({
+    //        toRecipients: ["mathis.merme@gmail.com"],
+    //        subject: "phishing report: \"" + phishSubject + "\"",
+    //        htmlBody: 'test',
+    //        attachments: [
+    //            { type: "item", itemId: phishItemId, name: phishSubject }
+    //        ],
+    //    },
+    //        function (asyncResult) {
+    //            var newMessage = asyncResult.value;
+    //            newMessage.sendAsync();
+    //        }
+    //    );
+    //}
+
     function composemail() {
-        Office.context.mailbox.displayNewMessageForm({
-            toRecipients: ["mathis.merme@gmail.com"],
-            ccRecipients: ["mathis.merme@groupe.schmidt"],
-            subject: "phishing report: \"" + phishSubject + "\"",
-            htmlBody: 'test',
-            attachments: [
-                { type: "item", itemId: phishItemId, name: phishSubject }
-            ],
-        },
+        Office.context.mailbox.displayNewMessageFormAsync(
+            {
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Phishing report: \"" + phishSubject + "\"",
+                htmlBody: 'testtttttttttttttttttttt',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+            },
             function (asyncResult) {
                 var newMessage = asyncResult.value;
+                // Ajout de l'événement "send" sur le nouveau message
+                newMessage.addHandlerAsync(Office.EventType.ItemSend, sendMessage);
+                // Envoi du message
                 newMessage.sendAsync();
             }
         );
     }
+
 
 
    
