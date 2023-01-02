3649f61c79ec7dc17f0338a80bf6c969102b897b
Shidooh <100126517+Shidooh@users.noreply.github.com>
2023-01-02 10:03:06 +0100

    Add files via upload

---
+++ FunctionFile.js
@@ -0,0 +1,34 @@
+'use strict';
+(function () {
+    Office.onReady(function () {
+        $(document).ready(function () {
+            getPhishingItem(Office.context.mailbox.item);
+            composemail();
+        });
+    });
+
+    var phishItemId;
+    var phishSubject;
+
+    function getPhishingItem(item) {
+        phishItemId = item.itemId
+        phishSubject = item.subject
+    }
+
+    function composemail() {
+    office.context.mailbox.displaynewmessageformasync(
+        {
+            torecipients: ["mathis.merme@gmail.com"],
+            subject: "phishing report: \"" + phishsubject + "\"",
+            htmlbody: 'test',
+            attachments: [{ type: "item", itemid: phishitemid, name: phishsubject }],
+        },
+        function (asyncresult) {
+            var newmessage = asyncresult.value;
+            // ajout de l'événement "send" sur le nouveau message
+            newmessage.addhandlerasync(office.eventtype.itemsend, sendmessage);
+        }
+    );
+}
+
+})();
