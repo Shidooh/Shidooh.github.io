8e516f04c76bd566556ea72f36972542361b924d
Shidooh <100126517+Shidooh@users.noreply.github.com>
2023-01-03 09:45:41 +0100

    Add files via upload

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -15,35 +15,35 @@
         phishSubject = item.subject
     }
 
-    //function composeMail() {
-    //    Office.context.mailbox.displayNewMessageFormAsync(
-    //        {
-    //            toRecipients: ["mathis.merme@gmail.com"],
-    //            subject: "Phishing report: \"" + phishSubject + "\"",
-    //            htmlBody: 'testt',
-    //            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
-    //            saveToSentItems: true
-    //        },
-    //    );
-    //}
-
-
     function composeMail() {
-        var email = {
-            toRecipients: ["mathis.merme@gmail.com"],
-            subject: "Phishing report: \"" + phishSubject + "\"",
-            htmlBody: 'test',
-            attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }]
-        };
-
-        Office.context.mailbox.item.sendAsync(email, {
-            saveToSentItems: true
-        }, function (result) {
-            if (result.status == "failed") {
-                console.log("L'envoi du message a échoué : " + result.error.message);
-            }
-        });
+        Office.context.mailbox.displayNewMessageFormAsync(
+            {
+                toRecipients: ["mathis.merme@gmail.com"],
+                subject: "Phishing report: \"" + phishSubject + "\"",
+                htmlBody: 'testt',
+                attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }],
+                saveToSentItems: true
+            },
+        );
     }
+
+
+    //function composeMail() {
+    //    var email = {
+    //        toRecipients: ["mathis.merme@gmail.com"],
+    //        subject: "Phishing report: \"" + phishSubject + "\"",
+    //        htmlBody: 'test',
+    //        attachments: [{ type: "item", itemId: phishItemId, name: phishSubject }]
+    //    };
+
+    //    Office.context.mailbox.item.sendAsync(email, {
+    //        saveToSentItems: true
+    //    }, function (result) {
+    //        if (result.status == "failed") {
+    //            console.log("L'envoi du message a échoué : " + result.error.message);
+    //        }
+    //    });
+    //}
 
 
 
